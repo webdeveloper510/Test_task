c0420a541cdd45ceb11de0ba7d672114fd80bfe0
abhishek <developerweb6@gmail.com>
2021-11-24 21:38:24 +0530

    changes

--- src/allPostspage.js
+++ src/allPostspage.js
@@ -1,22 +1,26 @@
-import React ,{ Component } from "react";
-import {Text, View,Image ,FlatList} from "react-native";
+import React, { Component } from "react";
+import { Text, View, Image, FlatList } from "react-native";
 import "./style.css";
-class Post extends Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-  return(
-      <View > 
-            <div className="post1">
-          <Image source={{uri: 'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg'}} style={{width: '100%', height: 350,padding:'30px'}} />   
-      <Text><h4>{this.props.route.params.details.title} </h4></Text><br/>
-      </div>
-        <div className="postbody1" >
-        <Text>{this.props.route.params.details.body}</Text>
+class Post extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <View >
+        <div className="post1">
+          <Image source={{ uri: 'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg' }} style={{ width: '100%', height: 350, padding: '30px' }} />
+          <Text>
+            <h4>{this.props.route.params.details.title}</h4>
+          </Text> <br />
         </div>
-        </View>
-  )
-    }
-} 
+        <div className="postbody1" >
+          <Text>
+            {this.props.route.params.details.body}
+          </Text>
+        </div>
+      </View>
+    )
+  }
+}
 export default Post;
--- src/post.js
+++ src/post.js
@@ -1,59 +1,53 @@
-import React ,{ Component } from "react";
-import { Text, View,Image ,FlatList,StyleSheet} from "react-native";
+import React, { Component } from "react";
+import { Text, View, Image, FlatList } from "react-native";
 import "./style.css";
-class AllPosts extends Component{
-    constructor(props){
-        super(props)
-            this.state = {
-         data :[]
+class AllPosts extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
     };
-    }
-   async componentDidMount(){
-        let response = await fetch(
-            'https://jsonplaceholder.typicode.com/posts'
-          );
-          let data1= await response.json();
-           this.setState({
-               data:data1,
-               viewableItems:''
-           })
-    } 
-    postPage=(item)=>{
-        this.props.navigation.navigate('Post Detail', { details: item })
-    }
-    onViewableItemsChanged = ({ viewableItems, changed }) =>this.setState({viewableItems})
-    render(){
-  return(
-    <View>
-    <div>
-    <FlatList style={{backgroundColor:"#ffff"}}
-    data={this.state.data}
-    renderItem={({item})=>  <View >
-           <div  onClick={()=>this.postPage(item)}>
-               <div className="post">
-            <Image source={{uri: 'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg'}} style={{width: 60, height: 60 ,borderRadius: '50%'}} />   
-               <Text><h4>{item.title} </h4></Text><br/>
-               </div>
-               <div className="postbody" >
-                 <Text>{item.body}</Text>
-                 </div>
-            </div>
-         </View> }
-    keyExtractor={item => item.id}
-    ref={r=>this.refs=r}
-    
-   
-  />
-    </div>
-  </View>
-  )
-    }
-} 
-const Style= StyleSheet.create({
-    postbody : {
-        marginLeft: '75px',
-        padding: '10px'
-    },
-  
-})
+  }
+  async componentDidMount() {
+    let response = await fetch(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    let data1 = await response.json();
+    this.setState({
+      data: data1,
+      viewableItems: ''
+    })
+  }
+  postPage = (item) => {
+    this.props.navigation.navigate('Post Detail', { details: item })
+  }
+  onViewableItemsChanged = ({ viewableItems, changed }) => this.setState({ viewableItems })
+  render() {
+    return (
+      <View>
+        <div>
+          <FlatList style={{ backgroundColor: "#ffff" }}
+            data={this.state.data}
+            renderItem={({ item }) => <View >
+              <div onClick={() => this.postPage(item)}>
+                <div className="post">
+                  <Image source={{ uri: 'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg' }} style={{ width: 60, height: 60, borderRadius: '50%' }} />
+                  <Text><h4>{item.title} </h4></Text><br />
+                </div>
+                <div className="postbody" >
+                  <Text>{item.body}</Text>
+                </div>
+              </div>
+            </View>}
+            keyExtractor={item => item.id}
+            ref={r => this.refs = r}
+
+
+          />
+        </div>
+      </View>
+    )
+  }
+}
+
 export default AllPosts;
